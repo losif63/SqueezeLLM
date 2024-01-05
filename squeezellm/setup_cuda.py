5e03b5e99d0c63457542fbe7f08e44bba8ca8d1c
losif63 <jaduksuh@naver.com>
2024-01-05 08:51:32 +0000

    Trying to incorporate bf16, compilation is impossible

--- squeezellm/setup_cuda.py
+++ squeezellm/setup_cuda.py
@@ -1,10 +1,12 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
+import sys
 
 setup(
     name='quant_cuda',
     ext_modules=[cpp_extension.CUDAExtension(
-        'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu']
+        name='quant_cuda', 
+        sources=['quant_cuda.cpp', 'quant_cuda_kernel.cu']
     )],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
